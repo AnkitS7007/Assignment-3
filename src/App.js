10f5d9f61be38451af2f2aef4742ab78cb44364c
Ankit Sharma <ankitbond7007@gmail.com>
2023-02-15 19:32:41 +0530

    assignment3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    } else {
+      setCount(10);
+    }
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+    }
+  };
+
+  let color;
+
+  if (count <= 4) {
+    color = "green";
+  } else if (count >= 5 && count <= 9) {
+    color = "blue";
+  } else if (count === 10) {
+    color = "red";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="counter">
+        <h1 style={{ color }}>{count}</h1>
+      </div>
+      <div className="btn">
+        <button onClick={increment}>Increment</button>
+        <button className="btn2" onClick={decrement}>Decrement</button>
+      </div>
+      <h3><i>Asignment done by Ankit Sharma - 12016468</i></h3>
+    </>
   );
-}
-
-export default App;
+};
+export default Counter;
